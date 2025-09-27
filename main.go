2149518e8a06d72ab64357f615c42c9690873f66
bryxosmmm <bryxosmmm@proton.me>
2025-09-27 13:27:58 +0300

    trying

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func Handler(w http.ResponseWriter, r *http.Request) {
 	_, err := w.Write([]byte("Hello, World"))
 	if err != nil {
-		log.Fatal("Zalupa")
+		log.Fatal("ERROR: failed to write: Hello, World")
 	}
 }
 
@@ -17,6 +17,6 @@ func main() {
 	fmt.Println("Hello world")
 	http.HandleFunc("/ping", Handler)
 	if http.ListenAndServe("127.0.0.1:80", nil) != nil {
-		log.Fatal("Failed to start server")
+		log.Fatal("ERROR: Failed to start server")
 	}
 }
